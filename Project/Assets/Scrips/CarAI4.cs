7d21d485e0851f06b60f1218a1143e16aaff489d
Corallo <dellisanti.angelo@gmail.com>
2020-02-28 10:34:25 +0100

    Add files via upload

--- Project/Assets/Scrips/CarAI4.cs
+++ Project/Assets/Scrips/CarAI4.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 using System;
 
 
-namespace UnityStandardAssets.Vehicles.Car
-{
+namespace UnityStandardAssets.Vehicles.Car {
     [RequireComponent(typeof(CarController))]
-    public class CarAI4 : MonoBehaviour
-    {
+    public class CarAI4 : MonoBehaviour {
         private CarController m_Car; // the car controller we want to use
 
         public GameObject terrain_manager_game_object;
@@ -16,9 +14,31 @@ namespace UnityStandardAssets.Vehicles.Car
 
         public GameObject[] friends;
         public GameObject[] enemies;
+        public GameObject leader;
+        public List<Vector3> positions;
+        int myIdx;
 
-        private void Start()
-        {
+        public int my_path_length;
+
+        public LayerMask my_mask;
+
+        public int skipper;
+
+        int counter = 0;
+        //SphereCollider droneCollider;
+        public float Margin = 1f;
+        public float sideMargin = 1f;
+
+        //All edges have same length:
+        public float edgeLength = 10.0f;
+        public float edgeMinDist = 0.1f;
+        public float addEdgeMaxLength = 12.0f;
+        int randomTimer = 0;
+        Vector3 goal_pos;
+        public int lastPointInPath;
+        public float newAngle = 0;
+        private Boolean backing = false;
+        private void Start() {
             // get the car controller
             m_Car = GetComponent<CarController>();
             terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
@@ -28,72 +48,284 @@ namespace UnityStandardAssets.Vehicles.Car
             // but for initial planning they should work
             friends = GameObject.FindGameObjectsWithTag("Player");
             enemies = GameObject.FindGameObjectsWithTag("Enemy");
-
-
-
+            leader = friends[0];
+            List<Vector3> positions = new List<Vector3>();
+            int i = 0;
+            foreach (var friend in friends) {
+                if (friend == this.gameObject) {
+                    myIdx = i;
+                }
+                i++;
+            }
             // Plan your path here
             // ...
+
+
         }
 
 
-        private void FixedUpdate()
-        {
+        private void FixedUpdate() {
 
-
-            // Execute your path here
-            // ...
-
-            Vector3 avg_pos = Vector3.zero;
-
-            foreach (GameObject friend in friends)
-            {
-                avg_pos += friend.transform.position;
+            float width = 20;
+            if (positions.Count < 50) {
+                positions.Add(leader.transform.position);
+            } else {
+                positions.RemoveAt(0);
+                positions.Add(leader.transform.position);
             }
-            avg_pos = avg_pos / friends.Length;
-            Vector3 direction = (avg_pos - transform.position).normalized;
+            RaycastHit raycastHitleft;
+            RaycastHit raycastHitright;
+            bool hitleft;
+            bool hitright;
+            float dl = width;
+            float dr = width;
 
-            bool is_to_the_right = Vector3.Dot(direction, transform.right) > 0f;
-            bool is_to_the_front = Vector3.Dot(direction, transform.forward) > 0f;
+            Vector3 line = createPerpendicularV(positions[0], positions[1]);
+            Vector3 goal = positions[0];
+            hitleft = Physics.Raycast(positions[0], line, out raycastHitleft, width, my_mask);
+            hitright = Physics.Raycast(positions[0], line, out raycastHitright, width, my_mask);
 
-            float steering = 0f;
-            float acceleration = 0;
 
-            if (is_to_the_right && is_to_the_front)
-            {
-                steering = 1f;
-                acceleration = 1f;
+            if (hitleft) {
+                Vector3 closest = raycastHitleft.collider.ClosestPointOnBounds(positions[0]);
+                dl = Vector3.Distance(closest, positions[0]);
+
             }
-            else if (is_to_the_right && !is_to_the_front)
-            {
-                steering = -1f;
-                acceleration = -1f;
+            if (hitright) {
+                Vector3 closest = raycastHitright.collider.ClosestPointOnBounds(positions[0]);
+                dr = Vector3.Distance(closest, positions[0]);
+
             }
-            else if (!is_to_the_right && is_to_the_front)
-            {
-                steering = -1f;
-                acceleration = 1f;
-            }
-            else if (!is_to_the_right && !is_to_the_front)
-            {
-                steering = 1f;
-                acceleration = -1f;
+            if (dl < dr) {
+                width = dl;
+            } else if (dr > dl) {
+                width = dr;
+            } else if (dl == dr) {
+                width = Math.Min(width, dl);
             }
 
-            // this is how you access information about the terrain
-            int i = terrain_manager.myInfo.get_i_index(transform.position.x);
-            int j = terrain_manager.myInfo.get_j_index(transform.position.z);
-            float grid_center_x = terrain_manager.myInfo.get_x_pos(i);
-            float grid_center_z = terrain_manager.myInfo.get_z_pos(j);
+            if (myIdx == 1) {
+                goal = positions[0] + width * line;
+            } else if (myIdx == 2) {
+                goal = positions[0] + 0.5f * width * line;
+            } else if (myIdx == 3) {
+                goal = positions[0] - 0.5f * width * line;
+            } else if (myIdx == 4) {
+                goal = positions[0] - width * line;
+            }
+            my_mask = LayerMask.GetMask("CubeWalls");
 
-            Debug.DrawLine(transform.position, new Vector3(grid_center_x, 0f, grid_center_z));
+            //if (my_path.Count == 0 || my_path.Count == 1) { return; }
+            /*foreach (Node n in my_path)
+            {
+                Debug.Log(n.getId());
+            }*/
+            CarController controller = transform.GetComponent<CarController>();
+            /*if (lastPointInPath == my_path.Count - 3)
+            {
+				Debug.Log("Goal position in sight!");
+				Debug.Log(goal_pos);
+                Vector3 carToTarget = transform.InverseTransformPoint(goal_pos);
+                float newSteer = (carToTarget.x / carToTarget.magnitude);
+                float newSpeed = 1f;
+                m_Car.Move(newSteer, newSpeed, newSpeed, 0);
+            }*/
+
+            Margin = 1;
+            sideMargin = 1;
+
+            if (skipper > 0) {
+                skipper--;
+                m_Car.Move(0f, 0f, 0f, 0f);
+                return;
+
+            }
+
+
+
+            //float curveAngel = Vector3.Angle(my_path[lastPointInPath].getPosition() - my_path[lastPointInPath + 1].getPosition(), my_path[lastPointInPath + 1].getPosition() - my_path[lastPointInPath + 2].getPosition());
+            /*int oldLastPointInPath = lastPointInPath;
+            for (int i = lastPointInPath; i < my_path.Count; i = i + 1)
+            {
+                if (3.0f + 0.04 * (controller.CurrentSpeed) >= Vector3.Distance(my_path[i].getPosition(), transform.position)) //0.04
+                //if (10.0f >= Vector3.Distance(my_path[i].getPosition(), transform.position)) 
+                {
+                    //lastPointInPath = i;
+                    if(oldLastPointInPath + 20 > i){
+                        lastPointInPath = i;
+                    }
+                }
+            }*/
+
+            /*
+            if (Vector3.Distance(transform.position, my_path[lastPointInPath].getPosition())<1f)
+            {
+                lastPointInPath++;
+            }
+            */
+
+
+
+            Vector3 target = goal;
+            float distanceToTargetTemp = Vector3.Distance(transform.position, target);
+            int targetId = 0;
+            /*for (int i = lastPointInPath + 2; i < my_path.Count; i = i + 1)
+            {
+                float newDistance = Vector3.Distance(transform.position, my_path[i].getPosition());
+                if (distanceToTargetTemp > newDistance)
+                {
+                    distanceToTargetTemp = newDistance;
+                    target = my_path[i].getPosition();
+                    targetId = i;
+                }
+            }*/
+
+            Vector3 carToTarget = transform.InverseTransformPoint(target);
+            float newSteer = (carToTarget.x / carToTarget.magnitude);
+            float newSpeed = 1f;
+            float handBreak = 0f;
+            Vector3 steeringPoint = new Vector3(0, 0, 1);
+            steeringPoint = (transform.rotation * steeringPoint);
+
+            bool carInFront = false;
+            bool carBehind = false;
+            
+            Vector3 rayFront = transform.position + steeringPoint;
+
+            float breakingDistance = (controller.CurrentSpeed * controller.CurrentSpeed) / (2);
+            float distanceToTarget = Vector3.Distance(transform.position, target);
+            if (distanceToTarget < 10f) {
+                newSpeed= 1f - ((10f - distanceToTarget)/10f);
+            }
+            RaycastHit rayHit;
+            bool hitBreak = Physics.SphereCast(rayFront, sideMargin, steeringPoint, out rayHit, 0.08f * breakingDistance,my_mask);
+
+
+            if (hitBreak && rayHit.collider.attachedRigidbody != null && rayHit.collider.attachedRigidbody.tag == "Player") {
+                Debug.Log("Player Detected.");
+                carInFront = true;
+            }
+
+            bool hitBack = Physics.SphereCast(rayFront, sideMargin, steeringPoint, out rayHit, Margin, my_mask);
+
+            if (hitBack && rayHit.collider.attachedRigidbody != null && rayHit.collider.attachedRigidbody.tag == "Player") {
+                Debug.Log("Player Detected.");
+                carInFront = true;
+            }
+
+            bool hitContinueBack = Physics.SphereCast(rayFront, sideMargin, steeringPoint, out rayHit, Margin * 3, my_mask);
+
+            if (hitContinueBack && rayHit.collider.attachedRigidbody != null && rayHit.collider.attachedRigidbody.tag == "Player") {
+                Debug.Log("Player Detected.");
+                carInFront = true;
+            }
+
+            RaycastHit rayBackHit;
+            bool hitFront = Physics.Raycast(transform.position - steeringPoint, -steeringPoint, out rayBackHit, 1, my_mask);
+
+            if (hitFront && rayBackHit.collider.attachedRigidbody != null && rayBackHit.collider.attachedRigidbody.tag == "Player") {
+                carBehind = true;
+            }
+
+            bool hitBreak_r = Physics.Raycast(transform.position, steeringPoint, 0.08f * breakingDistance, my_mask);
+            bool hitBack_r = Physics.Raycast(transform.position, steeringPoint, Margin, my_mask);
+            bool hitContinueBack_r = Physics.Raycast(transform.position, steeringPoint, Margin * 3, my_mask);
+
+            //newAngle = Vector3.Angle(transform.position - my_path[lastPointInPath + 1].getPosition(), my_path[lastPointInPath + 1].getPosition() - my_path[lastPointInPath + 2].getPosition());
+
+            /*if (controller.AccelInput == 0 && backing == false)
+            {
+                newSpeed = 1f / 1 + newAngle;
+            }*/
+            if (hitFront) {
+                newSpeed = newSpeed;
+                //Debug.Log("Ups");
+            } else if (hitBack || hitBack_r) {
+                //Debug.Log("AAAA");
+                backing = true;
+                newSpeed = -1f;
+                if (controller.BrakeInput > 0 && controller.AccelInput <= 0) {
+                    newSteer = -newSteer;
+                }
+            } else if ((hitBreak || hitBreak_r) && backing == false) {
+                //Debug.Log("Backing up");
+                newSpeed = -1;
+                //handBreak = 1;
+                // print("yes");
+
+            }
+            if ((hitContinueBack || hitContinueBack_r) && controller.AccelInput >= 0 && backing == false) {
+                //Debug.Log("BBBB");
+                newSteer = newSteer * 2;
+                //print("nope");
+            }
+            if ((hitContinueBack || hitContinueBack_r) && backing == true) {
+                //Debug.Log("CCCC");
+                newSpeed = -1f;
+                newSteer = -newSteer;
+                //print("yes");
+            } else {
+                backing = false;
+                //Debug.Log("DDDD");
+            }
+
+            if (controller.CurrentSpeed > 100) //Default 20
+            {
+                newSpeed = 0;
+            }
+
+            if (carToTarget.z / carToTarget.magnitude < 0) { // The point is behind the car
+                newSpeed = -1f;
+                newSteer = (-carToTarget.x / carToTarget.magnitude);
+                if (carToTarget.z / carToTarget.magnitude == -1) {
+                    newSpeed = -1f;
+                    newSteer = 1f; // Test. Unlikely to happen.
+                }
+            }
+
+            if (carInFront) {
+                Debug.Log("C A R ! ! ! ");
+                newSteer = 1f;
+            }
+
+            if (carBehind) {
+                newSteer = -1f;
+            }
+
+            m_Car.Move(newSteer, newSpeed, newSpeed, handBreak);
+
+
+
+
+            //Debug.DrawLine(transform.position,my_path[lastPointInPath+1].getPosition(), Color.black);
+            //Debug.DrawLine(my_path[lastPointInPath+1].getPosition(),my_path[lastPointInPath+2].getPosition(), Color.white);
+            Debug.DrawLine(transform.position, target, Color.black);
+            Debug.DrawLine(transform.position + steeringPoint * 0.08f * breakingDistance, transform.position, Color.white);
+
 
 
             // this is how you control the car
-            Debug.Log("Steering:" + steering + " Acceleration:" + acceleration);
-            m_Car.Move(steering, acceleration, acceleration, 0f);
-            //m_Car.Move(0f, -1f, 1f, 0f);
-
-
+            //m_Car.Move(0f, 0f, 0f, 0f);
         }
+
+
+
+
+
+        Vector3 createPerpendicularV(Vector3 a, Vector3 b) {
+
+            Vector2 a2 = new Vector2(a.x, a.z);
+            Vector2 b2 = new Vector2(b.x, b.z);
+            Vector2 c = b2 - a2;
+
+            c = Vector2.Perpendicular(c);
+            Vector3 c3 = new Vector3(c.x, 0, c.y);
+            c3 = Vector3.Normalize(c3);
+            return c3;
+        }
+
+
     }
 }
+    
+
